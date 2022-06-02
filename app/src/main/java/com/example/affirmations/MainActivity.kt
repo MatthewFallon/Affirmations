450d99d8a72d3c05ce0a93f80e6baba3de31a7df
Matthew Fallon <Matthew.b.fallon@gmail.com>
2022-06-02 15:28:08 -0500

    Finished codelab for basic structure of the app.

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
+    val myDataset = Datasource.loadAffirmations()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(findViewById<RecyclerView>(R.id.recycler_view)) {
+            adapter = ItemAdapter(context, myDataset)
+            setHasFixedSize(true)
+        }
     }
 }
